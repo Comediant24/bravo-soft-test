9acce7934a1aaf137f9d3ee7dc5149b146be7716
Iurii Bairamukov <juribai@mail.ru>
2021-05-16 12:27:23 +0700

    feat: создать логику обработки ошибок API
    Создать компонент ErrorApi
    
    Реализовать логику при отображения сообщения при любой ошибке сервера

---
+++ src/pages/main/components/errorApi/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.scss';
+const ErrorApi = () => {
+  return (
+    <div className="error">
+      <h3 className="error__title">Ошибка сервера. Попробуйте позже.</h3>
+    </div>
+  );
+};
+
+export default ErrorApi;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -6,9 +6,11 @@ import { createUser } from '../../adapters/createUser';
 import Table from './components/table';
 import './style.scss';
 import CreateForm from './components/createForm';
+import ErrorApi from './components/errorApi';
 
 const Main = () => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     getData();
@@ -17,50 +19,65 @@ const Main = () => {
   const getData = () => {
     return getUsersData()
       .then((users) => setData(users))
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        setError(true);
+        console.error(err);
+      });
   };
 
   const editData = (data) => {
-    const { id, firstName, lastName, age } = data;
-    return updateUser(id, { firstName, lastName, age })
+    const { id, firstName, lastName, city } = data;
+    return updateUser(id, { firstName, lastName, city })
       .then(() => getData())
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        setError(true);
+        console.error(err);
+      });
   };
 
   const removeData = (id) => {
-    removeUser(id)
+    return removeUser(id)
       .then(() => getData())
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        setError(true);
+        console.error(err);
+      });
   };
 
   const createData = (data) => {
-    const { firstName, lastName, age } = data;
-    return createUser({ firstName, lastName, age })
+    const { firstName, lastName, city } = data;
+    return createUser({ firstName, lastName, city })
       .then(() => getData())
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        setError(true);
+        console.error(err);
+      });
   };
 
   return (
-    <div className="main">
-      {!data && (
-        <h1 className="main__title main__title_load">Загрузка данных...</h1>
-      )}
-      {data && (
-        <>
-          <h1 className="main__title">Данные пользователей</h1>
-          <div className="main__btn-container">
-            <CreateForm input={data} createData={createData} />
-          </div>
-          <section className="main__content">
-            <Table
-              tableData={data}
-              editData={editData}
-              removeData={removeData}
-            />
-          </section>
-        </>
-      )}
-    </div>
+    <>
+      <div className="main">
+        {!data && !error && (
+          <h1 className="main__title main__title_load">Загрузка данных...</h1>
+        )}
+        {data && (
+          <>
+            <h1 className="main__title">Данные пользователей</h1>
+            <div className="main__btn-container">
+              <CreateForm input={data} createData={createData} />
+            </div>
+            <section className="main__content">
+              <Table
+                tableData={data}
+                editData={editData}
+                removeData={removeData}
+              />
+            </section>
+          </>
+        )}
+        {error && <ErrorApi error={error} />}
+      </div>
+    </>
   );
 };
 
